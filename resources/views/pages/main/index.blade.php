b76b761097d8ce91abbe7f443afcef6263fa8e2d
Val <89907932+ValerKob@users.noreply.github.com>
2023-10-08 22:32:46 +0300

    first commit

---
+++ resources/views/pages/main/index.blade.php
@@ -0,0 +1,196 @@
+@extends('layouts.main')
+
+@section('title', 'Главная страница')
+
+@section('content')
+
+<!--================ Start Pages Main =================-->
+<section class="oneBlog">
+    <div class="container">
+        <div class="rowOneBlog">
+            <div class="colOneBlogOne">
+                <div class="content">
+                    <div class="titleOneBlog">7-Я ШКОЛА!!!</div>
+                    <div class="textOneBlog">Её самый лучший, надёжный, добрый и
+                        уверенный коллектив...</div>
+                    <div class="btnOneBlog">
+                        <a href="#" class="linkOneBlog">Подробнее</a>
+                    </div>
+                </div>
+            </div>
+            <div class="colOneBlogTwo">
+                <div class="oneBlogSchool">
+                    <img class="imgOneBlog" src="../resources/images/MainPage/glavSchool.svg" alt="">
+                </div>
+
+                <div class="ellipseOneBlog">
+                    <div class="ellipseOneBlog1">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="175" height="175" viewBox="0 0 175 175" fill="none">
+                            <g filter="url(#filter0_d_6_32)">
+                                <circle cx="87.5" cy="87.5" r="37.5" fill="#E1C691" />
+                                <circle cx="87.5" cy="87.5" r="37.5" fill="#E1C691" />
+                                <circle cx="87.5" cy="87.5" r="37.5" fill="#E1C691" />
+                                <circle cx="87.5" cy="87.5" r="37.5" fill="#E1C691" />
+                            </g>
+                            <defs>
+                                <filter id="filter0_d_6_32" x="0" y="0" width="175" height="175" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                    <feMorphology radius="10" operator="dilate" in="SourceAlpha" result="effect1_dropShadow_6_32" />
+                                    <feOffset />
+                                    <feGaussianBlur stdDeviation="20" />
+                                    <feComposite in2="hardAlpha" operator="out" />
+                                    <feColorMatrix type="matrix" values="0 0 0 0 0.886275 0 0 0 0 0.709804 0 0 0 0 0.556863 0 0 0 1 0" />
+                                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_6_32" />
+                                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_6_32" result="shape" />
+                                </filter>
+                            </defs>
+                        </svg>
+                    </div>
+                    <div class="ellipseOneBlog2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="250" height="250" viewBox="0 0 250 250" fill="none">
+                            <g filter="url(#filter0_d_6_28)">
+                                <circle cx="125" cy="125" r="75" fill="#E1C691" />
+                                <circle cx="125" cy="125" r="75" fill="#E1C691" />
+                                <circle cx="125" cy="125" r="75" fill="#E1C691" />
+                                <circle cx="125" cy="125" r="75" fill="#E1C691" />
+                            </g>
+                            <defs>
+                                <filter id="filter0_d_6_28" x="0" y="0" width="250" height="250" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                    <feMorphology radius="10" operator="dilate" in="SourceAlpha" result="effect1_dropShadow_6_28" />
+                                    <feOffset />
+                                    <feGaussianBlur stdDeviation="20" />
+                                    <feComposite in2="hardAlpha" operator="out" />
+                                    <feColorMatrix type="matrix" values="0 0 0 0 0.886275 0 0 0 0 0.709804 0 0 0 0 0.556863 0 0 0 1 0" />
+                                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_6_28" />
+                                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_6_28" result="shape" />
+                                </filter>
+                            </defs>
+                        </svg>
+                    </div>
+                    <div class="ellipseOneBlog3">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="175" height="175" viewBox="0 0 175 175" fill="none">
+                            <g filter="url(#filter0_d_6_31)">
+                                <circle cx="87.5" cy="87.5" r="37.5" fill="#E1C691" />
+                                <circle cx="87.5" cy="87.5" r="37.5" fill="#E1C691" />
+                                <circle cx="87.5" cy="87.5" r="37.5" fill="#E1C691" />
+                                <circle cx="87.5" cy="87.5" r="37.5" fill="#E1C691" />
+                            </g>
+                            <defs>
+                                <filter id="filter0_d_6_31" x="0" y="0" width="175" height="175" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                    <feMorphology radius="10" operator="dilate" in="SourceAlpha" result="effect1_dropShadow_6_31" />
+                                    <feOffset />
+                                    <feGaussianBlur stdDeviation="20" />
+                                    <feComposite in2="hardAlpha" operator="out" />
+                                    <feColorMatrix type="matrix" values="0 0 0 0 0.886275 0 0 0 0 0.709804 0 0 0 0 0.556863 0 0 0 1 0" />
+                                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_6_31" />
+                                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_6_31" result="shape" />
+                                </filter>
+                            </defs>
+                        </svg>
+                    </div>
+                    <div class="ellipseOneBlog4">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="175" height="175" viewBox="0 0 175 175" fill="none">
+                            <g filter="url(#filter0_d_6_33)">
+                                <circle cx="87.5" cy="87.5" r="37.5" fill="#E1C691" />
+                                <circle cx="87.5" cy="87.5" r="37.5" fill="#E1C691" />
+                                <circle cx="87.5" cy="87.5" r="37.5" fill="#E1C691" />
+                                <circle cx="87.5" cy="87.5" r="37.5" fill="#E1C691" />
+                            </g>
+                            <defs>
+                                <filter id="filter0_d_6_33" x="0" y="0" width="175" height="175" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                    <feMorphology radius="10" operator="dilate" in="SourceAlpha" result="effect1_dropShadow_6_33" />
+                                    <feOffset />
+                                    <feGaussianBlur stdDeviation="20" />
+                                    <feComposite in2="hardAlpha" operator="out" />
+                                    <feColorMatrix type="matrix" values="0 0 0 0 0.886275 0 0 0 0 0.709804 0 0 0 0 0.556863 0 0 0 1 0" />
+                                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_6_33" />
+                                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_6_33" result="shape" />
+                                </filter>
+                            </defs>
+                        </svg>
+                    </div>
+                    <div class="ellipseOneBlog5">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="250" height="250" viewBox="0 0 250 250" fill="none">
+                            <g filter="url(#filter0_d_6_34)">
+                                <circle cx="125" cy="125" r="75" fill="#E1C691" />
+                                <circle cx="125" cy="125" r="75" fill="#E1C691" />
+                                <circle cx="125" cy="125" r="75" fill="#E1C691" />
+                                <circle cx="125" cy="125" r="75" fill="#E1C691" />
+                            </g>
+                            <defs>
+                                <filter id="filter0_d_6_34" x="0" y="0" width="250" height="250" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                    <feMorphology radius="10" operator="dilate" in="SourceAlpha" result="effect1_dropShadow_6_34" />
+                                    <feOffset />
+                                    <feGaussianBlur stdDeviation="20" />
+                                    <feComposite in2="hardAlpha" operator="out" />
+                                    <feColorMatrix type="matrix" values="0 0 0 0 0.886275 0 0 0 0 0.709804 0 0 0 0 0.556863 0 0 0 1 0" />
+                                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_6_34" />
+                                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_6_34" result="shape" />
+                                </filter>
+                            </defs>
+                        </svg>
+                    </div>
+                    <div class="ellipseOneBlog6">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="330" height="330" viewBox="0 0 330 330" fill="none">
+                            <g filter="url(#filter0_d_6_30)">
+                                <circle cx="165" cy="165" r="115" fill="#E1C691" />
+                                <circle cx="165" cy="165" r="115" fill="#E1C691" />
+                                <circle cx="165" cy="165" r="115" fill="#E1C691" />
+                                <circle cx="165" cy="165" r="115" fill="#E1C691" />
+                            </g>
+                            <defs>
+                                <filter id="filter0_d_6_30" x="0" y="0" width="330" height="330" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                    <feMorphology radius="10" operator="dilate" in="SourceAlpha" result="effect1_dropShadow_6_30" />
+                                    <feOffset />
+                                    <feGaussianBlur stdDeviation="20" />
+                                    <feComposite in2="hardAlpha" operator="out" />
+                                    <feColorMatrix type="matrix" values="0 0 0 0 0.886275 0 0 0 0 0.709804 0 0 0 0 0.556863 0 0 0 1 0" />
+                                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_6_30" />
+                                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_6_30" result="shape" />
+                                </filter>
+                            </defs>
+                        </svg>
+                    </div>
+                    <div class="ellipseOneBlog7">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" viewBox="0 0 300 300" fill="none">
+                            <g filter="url(#filter0_d_6_29)">
+                                <circle cx="150" cy="150" r="100" fill="#E1C691" />
+                                <circle cx="150" cy="150" r="100" fill="#E1C691" />
+                                <circle cx="150" cy="150" r="100" fill="#E1C691" />
+                                <circle cx="150" cy="150" r="100" fill="#E1C691" />
+                            </g>
+                            <defs>
+                                <filter id="filter0_d_6_29" x="0" y="0" width="300" height="300" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                    <feMorphology radius="10" operator="dilate" in="SourceAlpha" result="effect1_dropShadow_6_29" />
+                                    <feOffset />
+                                    <feGaussianBlur stdDeviation="20" />
+                                    <feComposite in2="hardAlpha" operator="out" />
+                                    <feColorMatrix type="matrix" values="0 0 0 0 0.886275 0 0 0 0 0.709804 0 0 0 0 0.556863 0 0 0 1 0" />
+                                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_6_29" />
+                                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_6_29" result="shape" />
+                                </filter>
+                            </defs>
+                        </svg>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+<!--================ End Pages Main =================-->
+
+@endsection('content')
